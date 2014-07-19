992a364fa3c873e23ec5d2409d2fbc2007a3ece9
adriandrew <adriandrew@hotmail.com>
2014-07-18 18:12:09 -0600

    Scroll infinito segunda version de prueba.
    Agregué los archivos donde corresponden, solo falta renombrar metodos y
    variables y unirlo al 100 con el metodo de MostrarImagenes3.

---
+++ MemexMonkey/AplicacionWeb/Js/modalLoad.js
@@ -0,0 +1,11 @@
+﻿
+function showLoad() {
+    $('#divLoadProgress').remove();
+
+    var $loadDiv = $('<div class="modalDialog" id="divLoadProgress" style="display: none;"><div><img src="Images/loading.gif"></div></div>').appendTo('body'); ;
+    $loadDiv.fadeIn();
+}
+
+function hideLoad() {
+    $('#divLoadProgress').remove();
+}
